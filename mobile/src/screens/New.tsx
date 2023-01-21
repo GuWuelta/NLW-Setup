dfabd6614acba23ffd93dce2ecc766dec7023700
GuWuelta <rm93449@fiap.com.br>
2023-01-21 16:17:56 -0300

    class 04: mobile finished

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -5,11 +5,13 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+import { api } from "../lib/axios";
 
 const avaiableWeekDays = [
   "Domingo",
@@ -23,6 +25,7 @@ const avaiableWeekDays = [
 
 export function New() {
   const [weekDays, setWeekDays] = useState<number[]>([]);
+  const [title, setTitle] = useState("");
 
   function handleToggleWeekDay(weekDayIndex: number) {
     if (weekDays.includes(weekDayIndex)) {
@@ -34,11 +37,32 @@ export function New() {
     }
   }
 
+  async function handleCreateNewHabit() {
+    try {
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert(
+          "Novo hábito",
+          "Informe o nome do hábito escolha a periodicidade."
+        );
+      }
+
+      await api.post("/habits", {title, weekDays})
+
+      setTitle("")
+      setWeekDays([])
+
+      Alert.alert("Novo hábito", "Hábito criado com sucesso")
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Ops", "Não foi possível criar um novo hábito");
+    }
+  }
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 100}}
+        contentContainerStyle={{ paddingBottom: 100 }}
       >
         <BackButton />
 
@@ -50,10 +74,12 @@ export function New() {
           Qual seu comprometimento?
         </Text>
 
-        <TextInput 
-            className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600" 
-            placeholder="Exercícios, Estudos, etc..."
-            placeholderTextColor={colors.zinc[400]}
+        <TextInput
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
+          placeholder="Exercícios, Estudos, etc..."
+          placeholderTextColor={colors.zinc[400]}
+          onChangeText={setTitle}
+          value={title}
         />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
@@ -70,18 +96,15 @@ export function New() {
         ))}
 
         <TouchableOpacity
-            className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
-            activeOpacity={0.7}
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
-          <Feather 
-            name="check"
-            size={20} 
-            color={colors.white} 
-        />
+          <Feather name="check" size={20} color={colors.white} />
 
-        <Text className="font-semibold text-base text-white ml-2">
+          <Text className="font-semibold text-base text-white ml-2">
             Confirmar
-        </Text>
+          </Text>
         </TouchableOpacity>
       </ScrollView>
     </View>
